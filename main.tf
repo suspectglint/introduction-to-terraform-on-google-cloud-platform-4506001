d3c399c30562a4d2eceb450533d19915afa6845e
suspectglint <47322410+suspectglint@users.noreply.github.com>
2025-07-18 08:08:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+resource "google_compute_network" "web" {
+  name                    = "web"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "web" {
+  name          = "web"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-west1"
+  network       = google_compute_network.web.id
+}
+
 data "google_compute_image" "ubuntu" {
   most_recent = true
   project     = "ubuntu-os-cloud" 
@@ -15,7 +27,7 @@ resource "google_compute_instance" "web" {
     }
   }
   network_interface {
-   subnetwork = "default"
+   subnetwork = "web"
    access_config {
       # Leave empty for dynamic public IP
     }
